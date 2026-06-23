9fdde536929269831d81dc9566c07f6d206d18d6
mollah2022 <sajibahmed12032022@gmail.com>
2026-06-23 10:46:51 +0600

    feat: add autocomplete API URL endpoint for location search

--- property_app/urls.py
+++ property_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('properties/', views.property_list, name='property_list'),
     path('properties/<slug:slug>/', views.property_detail, name='property_detail'),
+    path('api/locations/autocomplete/', views.location_autocomplete, name='location_autocomplete'),
 ]
